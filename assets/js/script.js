006291c619f84adf9e9b34927a673348327a0ecf
annia valdes <millerl.mb79@gmail.com>
2020-06-07 15:11:07 -0500

    end function generatePassword

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,8 @@ var confirmLowercase = "";
 var confirmNumeric = "";
 var confirmSpecial = "";
 
+// array for each password criteria
+
 var arrayUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"];
 var arrayLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
 var arrayNumeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -26,7 +28,7 @@ function criteriaPassword() {
 
   //validate no in letter and lenght between 8-128 chararter
 
-  if((chararterLenght=="") || (chararterLenght == nul| (chararterLenght < 8) || (chararterLenght > 128)) {
+  if((chararterLenght==="") || (chararterLenght ===null)|| (chararterLenght < 8) || (chararterLenght > 128)) {
     window.alert("You need to provide a valid answer! Please try again.");
     criteriaPassword();
 
@@ -37,10 +39,8 @@ function criteriaPassword() {
     confirmUppercase = confirm("Click OK to confirm including uppercase characters.");
     confirmLowercase = confirm("Click OK to confirm including lowercase characters.");
     confirmNumeric = confirm("Click OK to confirm including numeric character");
-    confirmSpecial = confirm("Click OK to confirm including special characters.");
-
-
-  }
+    confirmSpecial = confirm("Click OK to confirm including special characters.");      
+ }
 
 };
 
@@ -48,30 +48,64 @@ function criteriaPassword() {
 
 function generatePassword() {
 
-  // array for save the randow
-           
+  //call criteria fuction  
+  criteriaPassword();
+
+  // array (pass) for save a password  and array (arrayTotal) for save the  rest of the arrays confirm.. is true
+
   var pass = [];
-  var arrayTotal = []
+  var arrayTotal = [];
 
-// checked criteria the pasword
-
+  //checking entry criteria for Uppercase
+  // save a random value and add the array pass
+  
   if (confirmUppercase) {
     var ramUp = arrayUppercase[Math.floor(Math.random() * arrayUppercase.length - 1)];
-    
+    pass.push(ramUp);
+    arrayTotal = arrayTotal.concat(arrayUppercase);
+
   }
+   //checking entry criteria for Lowercase
+   // save a random value and add the array pass
+
   if (confirmLowercase) {
     var ramLo = arrayLowercase[Math.floor(Math.random() * arrayLowercase.length - 1)];
-    
-  if (confirmNumeric) {
-    var ramNu = arrayNumeric[Math.floor(Math.random() * arrayNumeric.length - 1)];
-   
-  if (confirmSpecial) {
-    var ramSp = arraySpecial[Math.floor(Math.random() * arraySpecial.length - 1)];
-   
+    pass.push(ramLo);
+    arrayTotal = arrayTotal.concat(arrayLowercase)
   }
 
-  
+   //checking entry criteria for Number
+   // save a random value and add the array pass
+
+  if (confirmNumeric) {
+    var ramNu = arrayNumeric[Math.floor(Math.random() * arrayNumeric.length - 1)];
+    pass.push(ramNu);
+    arrayTotal = arrayTotal.concat(arrayNumeric);
+  }
+
+   //checking entry criteria for Special chararter
+   // save a random value and add the array pass
+
+  if (confirmSpecial) {
+    var ramSp = arraySpecial[Math.floor(Math.random() * arraySpecial.length - 1)];
+    pass.push(ramSp);
+    arrayTotal = arrayTotal.concat(arraySpecial);
+  }
+   
+   // calculating the rest of characters to complete for the length pass
+
+  var rest = chararterLenght - pass.length;
+
+  // loop  different for complete the lenght chararter pass
+
+  for (var i = 0; i < rest; i++) {
+    var characterNew = arrayTotal[Math.floor(Math.random() * arrayTotal.length - 1)];
+    pass.push(characterNew);
+  }
+    //return a  only string 
+  return pass.join("");
 };
+
 
 
 
@@ -86,9 +120,6 @@ generateBtn.addEventListener("click", writePassword);
 // Write password to the #password input
 
 function writePassword() {
-
-   criteriaPassword();
-
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
